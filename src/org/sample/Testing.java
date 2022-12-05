9d1d3e2c0a4da204f0ac21d5f8a70ba2459e6b6e
subburaj036 <subburaj036@gmail.com>
2022-12-05 20:03:20 +0530

    First Project

---
+++ src/org/sample/Testing.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+public class Testing {
+	public static void main(String[] args) {
+		System.out.println("Test1");
+		
+		System.out.println("Test2");
+		
+		System.out.println("Test3");
+	}
+
+}
